aea0924b082868471e24b80819e56b95969fb4c2
webdevisme <nathan@webdevis.me>
2018-10-20 18:23:00 +0100

    Added new assets and played with pumpkin behavior
    Downloaded cemetery package and created the set for the game in spooky theme.
    
    Played with the pumpkin spawn and destroy - they now 'shuffle' away after a few seconds and the will be removed a few seconds later - to avoid spawning too many at once! (can be removed later when othere  mechanisms are employed)

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -18,9 +18,38 @@ public class Spawner : MonoBehaviour {
 
         position.x += Random.Range(rangeX * -1, rangeX);
         position.z += Random.Range(rangeZ * -1, rangeZ);
-
-        Instantiate(Resources.Load("pumpkin01"), position, gameObject.transform.rotation);
-
+        var pumpkinGO = Resources.Load("pumpkin01") as GameObject;
+        var pumpkin = Instantiate(pumpkinGO, position, gameObject.transform.rotation);
         StartCoroutine(SpawnPumpkin());
+        // wait for 5 before dissolve
+        yield return new WaitForSeconds(5);
+        //dissolve anim
+        StartCoroutine (this.MoveOverSeconds (pumpkin as GameObject, new Vector3 (0.0f, 0f, 1f), 1f));
+        // end of translate
+        Destroy(pumpkin,3.5f);
+
+        
     }
+
+    //   StartCoroutine (MoveOverSeconds (gameObject, new Vector3 (0.0f, 10f, 0f), 5f));
+        public IEnumerator MoveOverSpeed (GameObject objectToMove, Vector3 end, float speed){
+            // speed should be 1 unit per second
+            while (objectToMove.transform.position != end)
+            {
+                objectToMove.transform.position = Vector3.MoveTowards(objectToMove.transform.position, end, speed * Time.deltaTime);
+                yield return new WaitForEndOfFrame ();
+            }
+        }
+        public IEnumerator MoveOverSeconds (GameObject objectToMove, Vector3 end, float seconds)
+        {
+            float elapsedTime = 0;
+            Vector3 startingPos = objectToMove.transform.position;
+            while (elapsedTime < seconds)
+            {
+                objectToMove.transform.position = Vector3.Lerp(startingPos, end, (elapsedTime / seconds));
+                elapsedTime += Time.deltaTime;
+                yield return new WaitForEndOfFrame();
+            }
+            objectToMove.transform.position = end;
+        }
 }
