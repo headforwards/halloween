1095028c13edf8fac93b392ba1eb0ac30f41ff22
Barney Nicholls <barney.nicholls@axa-ppp.co.uk>
2018-10-30 11:12:40 +0000

    tweaked wurlitzer, not tested though :(

--- Assets/Scripts/Wurlitzer.cs
+++ Assets/Scripts/Wurlitzer.cs
@@ -3,83 +3,84 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class Wurlitzer : MonoBehaviour {
+public class Wurlitzer : MonoBehaviour
+{
 
-	Dictionary<string, float> cliplist = new Dictionary<string, float>();
+    Dictionary<string, float> cliplist = new Dictionary<string, float>();
 
-	AudioSource audio = null;
+    AudioSource audio = null;
 
-	public int MinPlayTime = 5;
-	public int MaxPlayTime = 10;
+    public int MinPlayTime = 5;
+    public int MaxPlayTime = 10;
 
-	float fadeRate = 0.5f;
-	float maxVolume = 0.5f;
+    float fadeRate = 0.5f;
+    float maxVolume = 0.5f;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
+        cliplist.Add("audio/dark ambient loop", 0.5f);
+        cliplist.Add("audio/Halloween-JT", 0.2f);
+        cliplist.Add("audio/01. Curious", 0.5f);
+        cliplist.Add("audio/02. Dark House", 0.5f);
+        cliplist.Add("audio/03. Woods", 1f);
 
-		cliplist.Add( "audio/dark ambient loop",0.5f);
-		cliplist.Add( 	"audio/Halloween-JT",0.2f);
-		cliplist.Add( 	"audio/01. Curious",0.5f);
-		cliplist.Add( 	"audio/02. Dark House",0.5f);
-		cliplist.Add( 	"audio/03. Woods",1f);
+        audio = gameObject.GetComponent<AudioSource>();
 
-		audio = gameObject.GetComponent<AudioSource>();
-		
-		startRandomTrack();
-	
-		StartCoroutine(fadeIn());
-	}
+        StartRandomTrack();
 
-	void startRandomTrack(){
-		// pick a track to start
-		int idx = (int) Mathf.Floor(Random.Range(0, cliplist.Count()));
-		var key = cliplist.Keys.ToArray()[idx];
-		audio.clip = Resources.Load<AudioClip>(key);
-		maxVolume = cliplist[key];
-		audio.volume = 0;
-		audio.Play();
-	}
+        StartCoroutine(FadeIn());
+    }
 
-	IEnumerator fadeIn() 
-	{
-		while (audio.volume  <= maxVolume) {
-			audio.volume = Mathf.Lerp( audio.volume, 1.0f, fadeRate * Time.deltaTime);
-			yield return new WaitForSeconds(0);
-		}
+    void StartRandomTrack()
+    {
+        int idx = (int)Mathf.Floor(Random.Range(0, cliplist.Count()));
+        var key = cliplist.Keys.ToArray()[idx];
+        audio.clip = Resources.Load<AudioClip>(key);
+        maxVolume = cliplist[key];
+        audio.volume = 0;
+        audio.Play();
+    }
 
-		StartCoroutine(playForAwhile());
-	 }
+    IEnumerator FadeIn()
+    {
+        while (audio.volume < maxVolume)
+        {
+            audio.volume = Mathf.Lerp(audio.volume, 1.0f, fadeRate * Time.deltaTime);
+            yield return new WaitForSeconds(0);
+        }
 
-	 IEnumerator playForAwhile()
-	 {
-		int playTime = (int) Mathf.Floor(Random.Range(MinPlayTime, MaxPlayTime));
+        StartCoroutine(PlayForAwhile());
+    }
 
-		yield return new WaitForSeconds(playTime);
+    IEnumerator PlayForAwhile()
+    {
+        int playTime = (int)Mathf.Floor(Random.Range(MinPlayTime, MaxPlayTime));
 
-		StartCoroutine(fadeOut());
-	 }
+        yield return new WaitForSeconds(playTime);
 
-	IEnumerator fadeOut() {
+        StartCoroutine(FadeOut());
+    }
 
-		while(audio.volume > 0.0){
-			audio.volume = Mathf.Lerp( audio.volume, 0.0f, fadeRate * Time.deltaTime );
-			yield return new WaitForSeconds(0);
-		}
+    IEnumerator FadeOut()
+    {
+        while (audio.volume > 0.0)
+        {
+            audio.volume = Mathf.Lerp(audio.volume, 0.0f, fadeRate * Time.deltaTime);
+            yield return new WaitForSeconds(0);
+        }
 
-		yield return null;
+        yield return null;
 
-		StartCoroutine(WaitToSwitchTracks());
-	 }
+        StartCoroutine(SwitchTracks());
+    }
 
-	IEnumerator WaitToSwitchTracks()
-	{
+    IEnumerator SwitchTracks()
+    {
+        yield return new WaitForSeconds(0);
 
-		yield return new WaitForSeconds(0);
+        StartRandomTrack();
 
-		startRandomTrack();
-
-		StartCoroutine(fadeIn());
-
-	}
+        StartCoroutine(FadeIn());
+    }
 }
